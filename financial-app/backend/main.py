52a1a7f02f7aff7e0d4c7ff9279ecbab2fb2d99b
Sylesh29 <82856450+Sylesh29@users.noreply.github.com>
2025-01-11 23:25:09 -0500

    Modified CORS

--- financial-app/backend/main.py
+++ financial-app/backend/main.py
@@ -4,14 +4,11 @@ import requests
 from fastapi.middleware.cors import CORSMiddleware
 import os
 
-
-
-
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
